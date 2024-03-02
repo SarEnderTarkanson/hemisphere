5e912fb4f401e749068f69fb6c9d5ecf312a34ec
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-03-02 03:39:16 +0100

    Transformed a functional component into a class-based component

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const HemisphereDisplay = () => {
-    return (
-        <div>
-            Hey you are in the Northern Hemisphere!
-        </div>
-    )
-}
+    return <div>Hey you are in the Northern Hemisphere!</div>;
+};
 
-export default HemisphereDisplay
+export default HemisphereDisplay;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => {
-
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    )
-    return(
-        <div>
-            Hello World!
-        </div>
-    )
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        );
+        return (
+            <div>
+                You are in the Northern Hemisphere!
+            </div>
+        )
+    }
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
