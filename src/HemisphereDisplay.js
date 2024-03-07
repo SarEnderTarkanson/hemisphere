1dba3a921432882b54dfe847dbee86c766f1ceb6
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-03-07 05:56:26 +0100

    Added the Pictures

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,8 +1,17 @@
 import React from "react";
+import northernPic from "./images/NorthernHemisphere.jpg";
+import southernPic from "./images/SouthernHemisphere.jpg";
 
 const HemisphereDisplay = ({ lattitude }) => {
-    const hemisphere = lattitude > 0 ? 'Northern hemisphere' : 'Southern hemipshere'
-    return <div>{hemisphere}</div>;
+  const hemisphere =
+    lattitude > 0 ? "Northern hemisphere" : "Southern hemipshere";
+  const picture = lattitude > 0 ? northernPic : southernPic;
+  return (
+    <div>
+      <img src={picture} alt="" />
+      {hemisphere}
+    </div>
+  );
 };
 
 export default HemisphereDisplay;
