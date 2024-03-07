6600eb10b2ff094906ccf30008ddb1bdac14f0a9
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-03-08 00:16:48 +0100

    Final version of this hands-on experience app

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,16 +1,34 @@
 import React from "react";
+import './Hemisphere.css'
 import northernPic from "./images/NorthernHemisphere.jpg";
 import southernPic from "./images/SouthernHemisphere.jpg";
 
+
+const hemisphereConfig = {
+  Northern: {
+    text: 'it is Northern Hemisphere',
+    picture: northernPic
+  },
+  Southern: {
+    text: 'it is southern Hemisphere',
+    picture: southernPic
+  }
+}
+
 const HemisphereDisplay = ({ lattitude }) => {
   const hemisphere =
-    lattitude > 0 ? "Northern hemisphere" : "Southern hemipshere";
-  const picture = lattitude > 0 ? northernPic : southernPic;
+    lattitude > 0 ? "Northern" : "Southern";
+  const { text, picture } = hemisphereConfig[hemisphere]
   return (
-    <div>
-      <img src={picture} alt="" />
-      {hemisphere}
+    <div className={hemisphere}>
+      <div className="ui raised text container segment">
+        <img src={picture} alt="" />
+      </div>
+      <div className="ui teal bottom attached label">
+        <h1>{text}</h1>
+      </div>
     </div>
+
   );
 };
 
